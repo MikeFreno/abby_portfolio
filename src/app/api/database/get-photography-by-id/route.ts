1d0c2a1c826a1561c9f46614b225b85e83312fd1
Michael Freno <michaelt.freno@gmail.com>
2023-09-26 02:36:39 -0400

    honestly impressed build process caught these

--- src/app/api/database/get-photography-by-id/route.ts
+++ src/app/api/database/get-photography-by-id/route.ts
@@ -3,7 +3,7 @@ import { ConnectionFactory } from "../ConnectionFactory";
 
 export async function GET() {
   const conn = ConnectionFactory();
-  const query = "SELECT FROM Project WHERE Type = ?";
+  const query = "SELECT * FROM Project WHERE Type = ?";
   const params = ["photography"];
   const results = await conn.execute(query, params);
   return NextResponse.json({ rows: results.rows }, { status: 200 });
